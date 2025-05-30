75e0126a6ff3b4dcaeac1945656868941e578a91
Sebastian359-cmd <sebastian.angeles359@gmail.com>
2025-05-29 19:41:37 -0500

    Add files via upload

---
+++ SistemaReservahotel_1/src/Logica/conexion.java
@@ -0,0 +1,36 @@
+
+package Logica;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+public class conexion {
+    public String db="basereserva";
+    public String url="jdbc:mysql://192.168.100.48/" +db;
+    public String user="usuario";
+    public String pass="usuario123";
+
+    public conexion() {
+    }
+    
+    public Connection conectar(){
+        Connection link=null;
+        
+        try {
+            Class.forName("org.gjt.mm.mysql.Driver");
+            link=DriverManager.getConnection(this.url, this.user, this.pass);
+            
+        } catch (ClassNotFoundException | SQLException e) {
+            JOptionPane.showConfirmDialog(null, e);
+            
+        }
+        
+        return link;
+    }
+    
+   
+    
+    
+}
